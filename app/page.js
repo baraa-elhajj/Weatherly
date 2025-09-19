e2fc934597c248749a78d05fde04e386f837fdfb
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-19 09:22:49 +0300

    Update page.js
    fetch data using axios

--- app/page.js
+++ app/page.js
@@ -1,4 +1,20 @@
+"use client";
 export default function Home() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+  const fetchWeather = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    axios.get(url).then((response) => {
+      setWeather(response.data);
+      console.log(response.data);
+    });
+    setCity("");
+    setLoading(false);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
