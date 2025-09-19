5888c25f70a0897ed54d79e6a49e118c96c0f90f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-19 09:23:16 +0300

    Update page.js
    missing imports

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 "use client";
+import axios from "axios";
+import { useState } from "react";
 export default function Home() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState({});
   const [loading, setLoading] = useState(false);
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=dubai&units=imperial&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
   const fetchWeather = (e) => {
     e.preventDefault();
     setLoading(true);
