f0bd045fa85cd91f25092a584e787bf79f0b9f50
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-22 00:31:07 +0300

    Update WeatherCard.jsx
    replaced dummy data with api response data

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -1,37 +1,44 @@
-export default function WeatherCard() {
+export default function WeatherCard({ data }) {
   return (
     <div className="flex justify-center items-center mt-50">
       <div className="bg-white/20 backdrop-blur-xl rounded-2xl shadow-xl p-6 w-[90%] sm:w-96 border border-white/30 text-white">
-        {/* City and date */}
         <div className="text-center mb-4">
-          <h2 className="text-2xl font-bold">London</h2>
-          <p className="text-sm opacity-80">Friday, September 19</p>
+          <h2 className="text-2xl font-bold">{data.name}</h2>
+          <p className="text-sm opacity-80">
+            {new Date().toLocaleDateString(undefined, {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
         </div>
 
-        {/* Temperature and icon */}
-        <div className="flex flex-col items-center pt">
-          <span className="text-6xl font-bold">22°C</span>
+        <div className="flex flex-col items-center">
+          <span className="text-6xl font-bold">
+            {Math.round(data.main.temp)}°C
+          </span>
           <img
-            src="https://openweathermap.org/img/wn/04d@2x.png"
-            alt="Cloudy"
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt={data.weather[0].description}
             className="w-20 h-20"
           />
-          <p className="text-lg mt-2">Cloudy</p>
+          <p className="text-lg mt-2 capitalize">
+            {data.weather[0].description}
+          </p>
         </div>
 
-        {/* Extra weather details */}
         <div className="grid grid-cols-3 gap-4 mt-6 text-center text-sm">
           <div className="flex flex-col">
             <span className="font-semibold">Humidity</span>
-            <span>65%</span>
+            <span>{data.main.humidity}%</span>
           </div>
           <div className="flex flex-col">
             <span className="font-semibold">Wind</span>
-            <span>15 km/h</span>
+            <span>{data.wind.speed} m/s</span>
           </div>
           <div className="flex flex-col">
             <span className="font-semibold">Feels</span>
-            <span>20°C</span>
+            <span>{Math.round(data.main.feels_like)}°C</span>
           </div>
         </div>
       </div>
