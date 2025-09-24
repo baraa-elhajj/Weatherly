f18560d9e973d757071bff3a359c989e215bb412
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-24 21:20:21 +0300

    Update layout.js
    imported varela font

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,9 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Varela_Round, Poppins } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const varela_round = Varela_Round({
+  weight: ["400"],
   subsets: ["latin"],
 });
 
@@ -20,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={varela_round.className}>
         {children}
         <Toaster position="top-center" reverseOrder={false} />
       </body>
