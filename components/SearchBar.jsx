1f1872f3d8d0c3ab8e852ec61031a5e67fac7d18
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-24 10:14:34 +0300

    Update SearchBar.jsx
    fix small screen search input

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -8,22 +8,22 @@ export default function SearchBar({
   inputRef,
 }) {
   return (
-    <div className="flex justify-center items-start pt-16 sm:pt-16 px-4 mt-3">
+    <div className="flex justify-center items-start pt-16 px-4 mt-3">
       <form
         onSubmit={onSubmit}
-        className="flex items-center w-[90%] sm:w-full max-w-md bg-transparent rounded-full shadow-lg border border-white/30 p-2 sm:p-3"
+        className="flex items-center w-full max-w-md bg-transparent rounded-full shadow-lg border border-white/30 p-2 sm:p-3"
       >
         <input
           ref={inputRef}
           type="text"
           placeholder="Search for a city"
-          className="flex-1 bg-transparent text-white placeholder-white/70  px-4 py-2 focus:outline-none text-sm sm:text-base"
+          className="flex-1 min-w-0 bg-transparent text-white placeholder-white/70 px-4 py-2 focus:outline-none text-sm sm:text-base"
           onChange={(e) => setCity(e.target.value)}
           value={city}
         />
         <button
           type="submit"
-          className="p-3 sm:text-base rounded-full bg-blue-400/80 text-white shadow-md transition-all hover:bg-blue-400 hover:scale-105 active:scale-95"
+          className="flex-shrink-0 p-3 sm:p-3 sm:text-base rounded-full bg-blue-400/80 text-white shadow-md transition-all hover:bg-blue-400 hover:scale-105 active:scale-95"
           disabled={loading}
         >
           <BsSearch className="text-xl" />
