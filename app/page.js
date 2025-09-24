ea94d5f964c8495c51e430589c24079869dd843f
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-24 08:59:05 +0300

    Update page.js
    suggestion logic

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import SearchBar from "@/components/SearchBar";
+import SuggestionsList from "@/components/SuggestionsList";
 import WeatherCard from "@/components/WeatherCard";
 import { capitalizeFirstLetter } from "@/utils/stringFormatter";
 import axios from "axios";
@@ -12,7 +13,16 @@ export default function Home() {
   const [city, setCity] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [suggestions, setSuggestions] = useState([]);
   const inputRef = useRef(null);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather`;
+  const citiesUrl = `https://api.openweathermap.org/geo/1.0/direct`;
+  const citiesParams = {
+    q: city,
+    limit: 5,
+    appid: process.env.NEXT_PUBLIC_WEATHER_KEY,
+  };
 
   useEffect(() => {
     const savedWeatherData = localStorage.getItem("weatherData");
@@ -25,12 +35,32 @@ export default function Home() {
     inputRef.current?.focus();
   }, []);
 
-  const url = `https://api.openweathermap.org/data/2.5/weather`;
-  const params = {
-    q: city,
-    units: "metric",
-    appid: process.env.NEXT_PUBLIC_WEATHER_KEY,
-  };
+  useEffect(() => {
+    if (!city) {
+      setSuggestions([]);
+      return;
+    }
+
+    const timeout = setTimeout(async () => {
+      await axios
+        .get(citiesUrl, { params: citiesParams })
+        .then((response) => {
+          console.log("getting suggestions for city ", city);
+          setSuggestions(response.data);
+          console.log("suggestions ", suggestions);
+        })
+        .catch((error) => {
+          console.error("Server Error:", error.response?.data);
+          toast.error(
+            capitalizeFirstLetter(
+              error.response?.data?.message || "Error fetching cities"
+            )
+          );
+        });
+    }, 300); // Debounce API calls for 300ms
+
+    return () => clearTimeout(timeout);
+  }, [city]);
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
@@ -44,7 +74,24 @@ export default function Home() {
     });
   };
 
-  const fetchWeather = async () => {
+  const handleOnSelect = (city) => {
+    console.log("selected ", city.name);
+    setCity(() => city.name);
+    setSuggestions([]);
+    fetchWeather(`${city.name},${city.country}`);
+  };
+
+  const fetchWeather = async (selectedCity) => {
+    const cityToFetch = selectedCity || city;
+    if (!cityToFetch) return;
+
+    const params = {
+      q: cityToFetch,
+      units: "metric",
+      appid: process.env.NEXT_PUBLIC_WEATHER_KEY,
+    };
+
+    console.log("fetching weather for ", city);
     setLoading(true);
     await axios
       .get(url, { params })
@@ -74,13 +121,18 @@ export default function Home() {
       />
       <div className="absolute inset-0 bg-black/40 z-0 pointer-events-none" />
 
-      <div className="relative z-20">
+      <div className="relative z-20 w-[90%] sm:w-full max-w-md mx-auto ">
         <SearchBar
           city={city}
           setCity={setCity}
           onSubmit={handleOnSubmit}
           loading={loading}
           inputRef={inputRef}
+        />
+
+        <SuggestionsList
+          suggestions={suggestions}
+          onSelect={(city) => handleOnSelect(city)}
         />
       </div>
 
