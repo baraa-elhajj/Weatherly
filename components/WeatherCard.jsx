58e2ac759098cab0594c8b2fa6e08b2acc13a3e0
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-25 05:27:14 +0300

    Update WeatherCard.jsx
    complete save city functionallity

--- components/WeatherCard.jsx
+++ components/WeatherCard.jsx
@@ -8,28 +8,28 @@ export default function WeatherCard({ data, savedCities, setSavedCities }) {
   const iconSrc = getIconSrc(data.weather?.[0]);
   const currentCity = `${data.name},${data.sys.country}`;
 
+  const handleButtonOnClick = () => {
+    if (!savedCities.includes(currentCity)) {
+      setSavedCities((prev) => {
+        const newSavedCities = [...prev, currentCity];
+        localStorage.setItem("savedCities", JSON.stringify(newSavedCities));
+        return newSavedCities;
+      });
+    } else {
+      setSavedCities((prev) => {
+        const newSavedCities = prev.filter((city) => city !== currentCity);
+        localStorage.setItem("savedCities", JSON.stringify(newSavedCities));
+        return newSavedCities;
+      });
+    }
+  };
+
   return (
     <div className="absolute inset-0 flex justify-center items-center mt-35">
       <div className="bg-blue-300/40 rounded-3xl shadow-2xl w-[90%] sm:w-96 mx-auto p-4 sm:p-6 text-white transform transition-all hover:scale-[1.02] hover:bg-blue-300/50">
         <div className="flex justify-end">
           <button
-            onClick={() => {
-              if (!savedCities.includes(currentCity)) {
-                setSavedCities((prev) => {
-                  const newSavedCities = [...prev, currentCity];
-                  console.log("Saved Cities list updated: ", newSavedCities);
-                  return newSavedCities;
-                });
-              } else {
-                setSavedCities((prev) => {
-                  const newSavedCities = prev.filter(
-                    (city) => city !== currentCity
-                  );
-                  console.log("Saved Cities list updated: ", newSavedCities);
-                  return newSavedCities;
-                });
-              }
-            }}
+            onClick={handleButtonOnClick}
             className="transform transition-all hover:scale-110 cursor-pointer"
           >
             {savedCities?.includes(currentCity) ? (
