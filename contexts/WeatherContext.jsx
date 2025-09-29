abfd1eace32e326b8f1410d953cae2cad94b3dd5
Baraa El Hajj <baraa.w.hajj@gmail.com>
2025-09-29 07:32:39 +0300

    Create WeatherContext.jsx

---
+++ contexts/WeatherContext.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+const WeatherContext = createContext();
+
+export function WeatherProvider({ children }) {
+  const [savedCities, setSavedCities] = useState([]);
+
+  useEffect(() => {
+    const savedCitiesLocal = localStorage.getItem("savedCities");
+    console.log("savedCitiesLocal: ", savedCitiesLocal);
+    if (savedCitiesLocal) {
+      const cities = JSON.parse(savedCitiesLocal);
+      console.log("cities: ", cities);
+      setSavedCities(cities);
+    }
+  }, []);
+
+  return (
+    <WeatherContext.Provider value={{ savedCities, setSavedCities }}>
+      {children}
+    </WeatherContext.Provider>
+  );
+}
+
+export const useWeatherContext = () => useContext(WeatherContext);
